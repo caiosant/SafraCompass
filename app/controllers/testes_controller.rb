7a353aa02ed67e72832ae16908978d3fddd7762d
Caio Sant'Ana da Silva <santanacaiio@gmail.com>
2022-04-02 20:06:51 -0300

    redo project

---
+++ app/controllers/testes_controller.rb
@@ -0,0 +1,9 @@
+class TestesController < ApplicationController
+    before_action :authenticate_user!
+    
+    def index
+        @users = User.all
+
+        render json: @users
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  devise_for :users,
+    controllers: {
+      registrations: :registrations,
+      sessions: :sessions
+    }
+
+  resources :testes, only: [:index]
+end
