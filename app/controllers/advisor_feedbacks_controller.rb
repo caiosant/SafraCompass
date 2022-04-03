e291ae5ce9fa5735a1ae5f08a30a579ba24524f6
Caio Sant'Ana da Silva <santanacaiio@gmail.com>
2022-04-03 14:12:02 -0300

    feat: feedback create/index/show

--- app/controllers/advisor_feedbacks_controller.rb
+++ app/controllers/advisor_feedbacks_controller.rb
@@ -1,4 +1,37 @@
 class AdvisorFeedbacksController < ApplicationController
+
+    def index
+        if current_advisor.present?
+            feedbacks = AdvisorFeedback.all.includes(:match).where(matches: {advisor_id: current_advisor.id})
+            
+            if current_advisor.id == feedbacks.first&.match&.advisor&.id && feedbacks.present?
+                render status: :ok, json: feedbacks.as_json
+            elsif feedbacks.present? and current_advisor&.id != feedbacks.first.match.advisor.id
+                render status: :unauthorized, json: { message: 'Há algo errado com sua autenticação.' }
+            else
+                render status: :no_content, json: { message: 'Objeto não encontrado' }
+            end
+        else
+            render status: :unauthorized, json: { message: 'Há algo errado com sua autenticação.' }
+        end
+    end
+
+    def show
+        if current_advisor.present? || current_user.present?
+            feedback = AdvisorFeedback.find(params[:id])
+
+            if feedback.present? && feedback.match.advisor_id == current_advisor&.id || feedback.match.user_id == current_user&.id
+                render status: :ok, json: feedback.as_json
+            elsif feedback.present? and current_advisor&.id != feedback.match.advisor_id || feedback.match.user_id == current_user&.id 
+                render status: :unauthorized, json: { message: 'Há algo errado com sua autenticação.' }
+            else
+                render status: :no_content, json: { message: 'Objeto não encontrado' }
+            end
+        else
+            render status: :unauthorized, json: { message: 'Há algo errado com sua autenticação.' }
+        end
+    end
+
     def create
         if current_user.present?
             feedback = AdvisorFeedback.new(feedback_params)
