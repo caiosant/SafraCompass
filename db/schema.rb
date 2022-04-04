5f57529390d2d0e1aa4bed6a18029a3e302550de
Caio Sant'Ana da Silva <santanacaiio@gmail.com>
2022-04-03 21:58:32 -0300

    feat: add postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_403_190_258) do
-  create_table 'advisor_feedbacks', force: :cascade do |t|
-    t.integer 'grade'
-    t.text 'comment'
-    t.integer 'match_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['match_id'], name: 'index_advisor_feedbacks_on_match_id'
+ActiveRecord::Schema[7.0].define(version: 2022_04_03_190258) do
+  create_table "advisor_feedbacks", force: :cascade do |t|
+    t.integer "grade"
+    t.text "comment"
+    t.integer "match_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["match_id"], name: "index_advisor_feedbacks_on_match_id"
   end
 
   create_table "advisor_profiles", force: :cascade do |t|
@@ -49,71 +47,71 @@ ActiveRecord::Schema[7.0].define(version: 20_220_403_190_258) do
     t.index ["advisor_id"], name: "index_advisor_profiles_on_advisor_id"
   end
 
-  create_table 'advisors', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_advisors_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_advisors_on_reset_password_token', unique: true
+  create_table "advisors", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_advisors_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_advisors_on_reset_password_token", unique: true
   end
 
-  create_table 'matches', force: :cascade do |t|
-    t.integer 'advisor_id', null: false
-    t.integer 'user_id', null: false
-    t.boolean 'active', default: true, null: false
-    t.datetime 'started_at'
-    t.datetime 'finished_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['advisor_id'], name: 'index_matches_on_advisor_id'
-    t.index ['user_id'], name: 'index_matches_on_user_id'
+  create_table "matches", force: :cascade do |t|
+    t.integer "advisor_id", null: false
+    t.integer "user_id", null: false
+    t.boolean "active", default: true, null: false
+    t.datetime "started_at"
+    t.datetime "finished_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["advisor_id"], name: "index_matches_on_advisor_id"
+    t.index ["user_id"], name: "index_matches_on_user_id"
   end
 
-  create_table 'user_profiles', force: :cascade do |t|
-    t.string 'name'
-    t.string 'last_name'
-    t.string 'social_name'
-    t.date 'birthday'
-    t.integer 'gender'
-    t.string 'adress'
-    t.string 'adress_number'
-    t.string 'adress_complement'
-    t.string 'country'
-    t.string 'state'
-    t.string 'city'
-    t.string 'born_in'
-    t.string 'cpf'
-    t.string 'rg'
-    t.string 'rg_issuer'
-    t.boolean 'deficient'
-    t.integer 'deficient_description'
-    t.string 'mom_name'
-    t.integer 'marital_status'
-    t.integer 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_user_profiles_on_user_id'
+  create_table "user_profiles", force: :cascade do |t|
+    t.string "name"
+    t.string "last_name"
+    t.string "social_name"
+    t.date "birthday"
+    t.integer "gender"
+    t.string "adress"
+    t.string "adress_number"
+    t.string "adress_complement"
+    t.string "country"
+    t.string "state"
+    t.string "city"
+    t.string "born_in"
+    t.string "cpf"
+    t.string "rg"
+    t.string "rg_issuer"
+    t.boolean "deficient"
+    t.integer "deficient_description"
+    t.string "mom_name"
+    t.integer "marital_status"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_profiles_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'advisor_feedbacks', 'matches'
-  add_foreign_key 'advisor_profiles', 'advisors'
-  add_foreign_key 'matches', 'advisors'
-  add_foreign_key 'matches', 'users'
-  add_foreign_key 'user_profiles', 'users'
+  add_foreign_key "advisor_feedbacks", "matches"
+  add_foreign_key "advisor_profiles", "advisors"
+  add_foreign_key "matches", "advisors"
+  add_foreign_key "matches", "users"
+  add_foreign_key "user_profiles", "users"
 end
